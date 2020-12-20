e1455c6a7d9a831d3a351da560b2f944e9c5d786
Leyton Valencia <lvalenciap@est.ups.edu.ec>
2020-12-20 16:52:33 -0500

    Commit inicial

---
+++ script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#Script de prueba par Git 
+#Autor: Valencia Leyton <lvalenciap@est.ups.edu.ec>
+#Fecha: 12-20-2020
+#Version: 1.0.0
+
+def main():
+    print("Hola Mundo!")
+
+if __name__ == "__main__":
+    main()
